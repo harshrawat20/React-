a04fb179852188fbe3daa1be312ca046722c0a6d
Harsh Rawat <20bcs050@iiitdwd.ac.in>
2024-01-16 13:23:42 +0530

    Changed Image url

--- tailwindprops/src/components/Card.jsx
+++ tailwindprops/src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({username,btnText="visit me"}) {
     return (
       <div className="relative h-[400px] w-[300px] rounded-md mb-4">
         <img
-          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          src="https://i.ibb.co/B6XhbWg/Harsh.jpg&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
           alt="AirMax Pro"
           className="z-0 h-full w-full rounded-md object-cover"
         />
